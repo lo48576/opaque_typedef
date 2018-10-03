ce14a34c666a71fb1cce0f3a32095c64e1e0451c
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-10-03 21:13:33 +0900

    Add an error type for builders

--- opaque_typedef_codegen/src/builder/mod.rs
+++ opaque_typedef_codegen/src/builder/mod.rs
@@ -1,5 +1,25 @@
 //! Builders.
 
+use std::error;
+use std::fmt;
+
 pub use self::generics::GenericsBuilder;
 
 mod generics;
+
+/// Builder error.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Error {
+    /// Required field is not set.
+    FieldNotSet(&'static str),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::FieldNotSet(field) => writeln!(f, "Required field is not set: `{}`", field),
+        }
+    }
+}
+
+impl error::Error for Error {}
